3ba99dbafa686dd2d33474c0b73057dba2666fd3
Bram den Teuling <bramdenteuling@gmail.com>
2012-08-19 23:32:01 +0200

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sterknetwerk::Application.config.secret_token = 'b1367d34fcc21a630c6cb68b1b1e8f06b569140cbe7a3ed5dd201425da4c0d4b6682751b29a60f5d02ec49c6c9b06bb52926d41e3defd01658dce9bf44cb9ccb'
