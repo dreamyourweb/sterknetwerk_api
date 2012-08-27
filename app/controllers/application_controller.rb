05f6694a803c5744857c3221fb9961a20d6df913
Bram den Teuling <bramdenteuling@gmail.com>
2012-08-27 22:37:03 +0200

    authentication bugs?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
   
   private
-  def authenticate_user
-    @current_user = User.find_by_authentication_token(params[:token])
-    unless @current_user
-     respond_with({:error => "Token is invalid." })
-    end
-  end
-  def current_user
-    @current_user
-  end
+  #def authenticate_user
+  #  @current_user = User.find_by_authentication_token(params[:token])
+  #  unless @current_user
+  #   respond_with({:error => "Token is invalid." })
+  #  end
+  #end
+  #def current_user
+  #  @current_user
+  #end
 end
