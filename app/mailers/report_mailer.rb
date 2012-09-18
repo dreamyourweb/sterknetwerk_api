0c266700501b36b74840b93b6b1e3487ce9bf144
Andres Lamont <alamont@gmail.com>
2012-09-18 16:00:13 +0200

    Fixed subject line

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -3,7 +3,7 @@ class ReportMailer < ActionMailer::Base
 
   def pdf_report(recipient_name, recipient_email, pdf_filename, company_name)
   	attachments['rapport.pdf'] = File.read(pdf_filename)
-    mail(:to => "#{recipient_name} <#{recipient_email}>", :bcc => "SterkNetwerk <info@sterknetwerk.nl>", :subject => company_name) do |format|
+    mail(:to => "#{recipient_name} <#{recipient_email}>", :bcc => "SterkNetwerk <info@sterknetwerk.nl>", :subject => "ICT analyse #{company_name}") do |format|
     	format.text { render :text => "Bedankt voor het invullen van de Sterknetwerk vragenlijst.\nNeem gerust contact op bij vragen.\n\nMet vriendelijke groet,\nSterknetwerk" }
     end
   end
